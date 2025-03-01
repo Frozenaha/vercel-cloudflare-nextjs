84d45daba2777c23691f70d83cd3495a2c4193a6
wwz <1255340634@qq.com>
2025-03-01 22:12:47 +0800

    c dev

--- src/app/c/page.tsx
+++ src/app/c/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 export const dynamic = "force-dynamic";
 export const revalidate = 3600;
 function C() {
-  return <div>C</div>;
+  return <div>C dev</div>;
 }
 
 export default C;
